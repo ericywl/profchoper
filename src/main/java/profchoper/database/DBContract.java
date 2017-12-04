2d05d8ceeb60afc9ae94595773ccfbb4aa186c47
ericywl <midnight2903@gmail.com>
2017-12-04 22:15:55 +0800

    trying out static dbsource

---
+++ src/main/java/profchoper/database/DBContract.java
@@ -0,0 +1,29 @@
+package profchoper.database;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+
+import javax.sql.DataSource;
+import java.sql.SQLException;
+
+public class DBContract {
+    @Value("${spring.datasource.url}")
+    public static String dbUrl;
+
+    @Autowired
+    public static DataSource dataSource;
+
+    @Bean
+    public DataSource dataSource() throws SQLException {
+        if (dbUrl == null || dbUrl.isEmpty()) {
+            return new HikariDataSource();
+        } else {
+            HikariConfig config = new HikariConfig();
+            config.setJdbcUrl(dbUrl);
+            return new HikariDataSource(config);
+        }
+    }
+}
---
+++ src/main/java/profchoper/user/StudentDB.java
@@ -0,0 +1,14 @@
+package profchoper.user;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+
+import javax.sql.DataSource;
+import java.sql.SQLException;
+
+public class StudentDB {
+
+}
