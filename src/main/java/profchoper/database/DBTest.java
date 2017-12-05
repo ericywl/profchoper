fec4006916ca5e16132533bbe526754b6bcf7267
ericywl <midnight2903@gmail.com>
2017-12-05 15:28:30 +0800

    nested list in index.html

--- src/main/java/profchoper/database/DBTest.java
+++ src/main/java/profchoper/database/DBTest.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import javax.sql.DataSource;
 import java.sql.*;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Map;
 
 @Controller
@@ -20,15 +21,26 @@ public class DBTest {
     String index(Map<String, Object> model) {
         try (Connection connection = dataSource.getConnection()) {
             Statement stmt = connection.createStatement();
-            ResultSet rs = stmt.executeQuery("SELECT * FROM students");
+            ResultSet rs1 = stmt.executeQuery("SELECT (id, name) FROM students");
+            ResultSet rs2;
 
-            ArrayList<String> output = new ArrayList<>();
-            while (rs.next()) {
-                output.add(+ rs.getInt("id")
-                        + ": " + rs.getString("name"));
+            ArrayList<String> studentOutput = new ArrayList<>();
+            ArrayList<ArrayList<String>> courseOutput = new ArrayList<>();
+            while (rs1.next()) {
+                studentOutput.add(rs1.getInt("id")
+                        + ": " + rs1.getString("name"));
+
+                rs2 = stmt.executeQuery("SELECT (course1, course2, course3, course4) FROM students");
+                String course1 = "" + rs2.getDouble("course1");
+                String course2 = "" + rs2.getDouble("course2");
+                String course3 = "" + rs2.getDouble("course3");
+                String course4 = "" + rs2.getDouble("course4");
+
+                ArrayList<String> temp = new ArrayList<>(Arrays.asList(course1, course2, course3, course4));
+                courseOutput.add(temp);
             }
 
-            model.put("students", output);
+            model.put("students", studentOutput);
             return "index";
         } catch (SQLException ex) {
             model.put("message", ex.getMessage());
