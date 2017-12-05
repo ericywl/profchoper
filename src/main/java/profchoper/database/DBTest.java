ca483cae12f748897b0b19c0a23572815f1c2fe7
ericywl <midnight2903@gmail.com>
2017-12-05 15:40:33 +0800

    lets try again

--- src/main/java/profchoper/database/DBTest.java
+++ src/main/java/profchoper/database/DBTest.java
@@ -21,20 +21,18 @@ public class DBTest {
     String index(Map<String, Object> model) {
         try (Connection connection = dataSource.getConnection()) {
             Statement stmt = connection.createStatement();
-            ResultSet rs1 = stmt.executeQuery("SELECT id, name FROM students");
-            ResultSet rs2;
+            ResultSet rs = stmt.executeQuery("SELECT * FROM students");
 
             ArrayList<String> studentOutput = new ArrayList<>();
             ArrayList<ArrayList<String>> courseOutput = new ArrayList<>();
-            while (rs1.next()) {
-                studentOutput.add(rs1.getInt("id")
-                        + ": " + rs1.getString("name"));
-
-                rs2 = stmt.executeQuery("SELECT course1, course2, course3, course4 FROM students");
-                String course1 = "" + rs2.getDouble("course1");
-                String course2 = "" + rs2.getDouble("course2");
-                String course3 = "" + rs2.getDouble("course3");
-                String course4 = "" + rs2.getDouble("course4");
+            while (rs.next()) {
+                studentOutput.add(rs.getInt("id")
+                        + ": " + rs.getString("name"));
+                
+                String course1 = "" + rs.getDouble("course1");
+                String course2 = "" + rs.getDouble("course2");
+                String course3 = "" + rs.getDouble("course3");
+                String course4 = "" + rs.getDouble("course4");
 
                 ArrayList<String> temp = new ArrayList<>(Arrays.asList(course1, course2, course3, course4));
                 courseOutput.add(temp);
