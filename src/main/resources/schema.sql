96377ac9cbeace8e2c96fbbcb8e1b9586cd97429
ericywl <midnight2903@gmail.com>
2017-12-05 15:15:37 +0800

    schema.sql error again

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,7 +1,9 @@
-CREATE TABLE IF NOT EXISTS students (
+CREATE TABLE public.students
+(
   id INTEGER PRIMARY KEY,
   name TEXT NOT NULL,
   course1 REAL,
   course2 REAL,
-  course3 REAL, 
-  course4 REAL)
+  course3 REAL,
+  course4 REAL
+);
