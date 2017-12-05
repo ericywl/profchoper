2199b8ad37deb869923389e63b0bfb3e7f9596a3
ericywl <midnight2903@gmail.com>
2017-12-06 04:59:40 +0800

    trying out some new stuff

--- src/main/java/profchoper/course/CourseRepository.java
+++ src/main/java/profchoper/course/CourseRepository.java
@@ -2,6 +2,7 @@ package profchoper.course;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
@@ -23,10 +24,11 @@ public class CourseRepository {
         return select.query(sql, new CourseRowMapper());
     }
 
-    public List<Course> findById(String id) throws SQLException {
+    public Course findById(String id) throws SQLException {
         JdbcTemplate select = new JdbcTemplate(dataSource);
         String sql = "SELECT * FROM courses WHERE id = ?";
 
-        return select.query(sql, new Object[]{id}, new CourseRowMapper());
+        return (Course) select.queryForObject(sql, new Object[]{id},
+                new BeanPropertyRowMapper(Course.class));
     }
 }
