fb7e8e2c04fb634cb0cae3a51d04137d98dbe2fc
ericywl <midnight2903@gmail.com>
2017-12-05 15:38:06 +0800

    should work now

--- src/main/java/profchoper/database/DBTest.java
+++ src/main/java/profchoper/database/DBTest.java
@@ -21,15 +21,27 @@ public class DBTest {
     String index(Map<String, Object> model) {
         try (Connection connection = dataSource.getConnection()) {
             Statement stmt = connection.createStatement();
-            ResultSet rs1 = stmt.executeQuery("SELECT * FROM students");
+            ResultSet rs1 = stmt.executeQuery("SELECT id, name FROM students");
+            ResultSet rs2;
 
             ArrayList<String> studentOutput = new ArrayList<>();
+            ArrayList<ArrayList<String>> courseOutput = new ArrayList<>();
             while (rs1.next()) {
                 studentOutput.add(rs1.getInt("id")
                         + ": " + rs1.getString("name"));
+
+                rs2 = stmt.executeQuery("SELECT course1, course2, course3, course4 FROM students");
+                String course1 = "" + rs2.getDouble("course1");
+                String course2 = "" + rs2.getDouble("course2");
+                String course3 = "" + rs2.getDouble("course3");
+                String course4 = "" + rs2.getDouble("course4");
+
+                ArrayList<String> temp = new ArrayList<>(Arrays.asList(course1, course2, course3, course4));
+                courseOutput.add(temp);
             }
 
             model.put("students", studentOutput);
+            model.put("courses", courseOutput);
             return "index";
         } catch (SQLException ex) {
             model.put("message", ex.getMessage());
