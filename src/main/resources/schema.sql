3698f63eb8a3fa91baf04ec16d9dd514ae49a82a
ericywl <midnight2903@gmail.com>
2017-12-05 20:03:14 +0800

    reordered courses

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -3,6 +3,12 @@ DROP TABLE IF EXISTS public.professors;
 DROP TABLE IF EXISTS public.bookings;
 DROP TABLE IF EXISTS public.courses;
 
+CREATE TABLE IF NOT EXISTS public.courses
+(
+  id TEXT PRIMARY KEY,
+  name TEXT NOT NULL
+);
+
 CREATE TABLE IF NOT EXISTS public.students
 (
   id INTEGER PRIMARY KEY,
@@ -19,12 +25,6 @@ CREATE TABLE IF NOT EXISTS public.professors
   alias TEXT UNIQUE,
   office TEXT UNIQUE,
   course_id TEXT REFERENCES courses(id)
-);
-
-CREATE TABLE IF NOT EXISTS public.courses
-(
-  id TEXT PRIMARY KEY,
-  name TEXT NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS public.bookings
