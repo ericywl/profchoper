a4aa76929e7c8f5f6c9ceae84b833268dcb59e74
ericywl <midnight2903@gmail.com>
2017-12-06 03:33:07 +0800

    added value to repo

--- src/main/java/profchoper/repository/CourseRepository.java
+++ src/main/java/profchoper/repository/CourseRepository.java
@@ -13,7 +13,7 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
-@Repository("courseRepository")
+@Repository(value = "courseRepository")
 public class CourseRepository {
     @Autowired
     @Qualifier("profChoperDataSource")
