e54fa905041cb47ea4323a3d102fa5ae05a001de
ericywl <midnight2903@gmail.com>
2017-12-06 20:38:31 +0800

    redirect

--- src/main/java/profchoper/security/ProfChoperAuthSuccessHandler.java
+++ src/main/java/profchoper/security/ProfChoperAuthSuccessHandler.java
@@ -10,7 +10,6 @@ import org.springframework.security.web.WebAttributes;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
 
-import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
@@ -29,8 +28,7 @@ public class ProfChoperAuthSuccessHandler implements AuthenticationSuccessHandle
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request,
                                         HttpServletResponse response,
-                                        Authentication authentication)
-            throws IOException, ServletException {
+                                        Authentication authentication) throws IOException{
 
         handle(request, response, authentication);
         clearAuthenticationAttributes(request);
--- src/main/java/profchoper/security/SecurityConfig.java
+++ src/main/java/profchoper/security/SecurityConfig.java
@@ -7,6 +7,7 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.web.access.AccessDeniedHandler;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 
 import javax.sql.DataSource;
 
@@ -23,20 +24,15 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     private AccessDeniedHandler accessDeniedHandler;
 
     @Autowired
+    private AuthenticationSuccessHandler successHandler;
+
+    @Autowired
     public void configAuthentication(AuthenticationManagerBuilder auth) throws Exception {
         auth.jdbcAuthentication()
                 .dataSource(dataSource)
                 .usersByUsernameQuery("SELECT username, password, enabled FROM users WHERE username = ?")
                 .authoritiesByUsernameQuery("SELECT username, role FROM user_roles WHERE username = ?");
     }
-
-    /*@Autowired
-    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication()
-                .withUser("student").password("password").roles(ROLE_STUDENT)
-                .and()
-                .withUser("prof").password("password").roles(ROLE_PROF);
-    }*/
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -47,7 +43,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers("/student/**").hasAnyRole(ROLE_STUDENT)
                 .anyRequest().authenticated()
                 .and()
-                .formLogin()
+                .formLogin().successHandler(successHandler)
                 .loginPage("/login")
                 .permitAll()
                 .and()
