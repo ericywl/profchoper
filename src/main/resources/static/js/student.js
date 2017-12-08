c11a35b9c531133c36c534738be04f5978ab3f8a
ericywl <midnight2903@gmail.com>
2017-12-09 00:19:27 +0800

    changed student.js

--- src/main/resources/static/js/student.js
+++ src/main/resources/static/js/student.js
@@ -2,41 +2,19 @@ $(document).ready(function () {
     $("table td").click(function () {
         $("#myModal").modal();
     });
-});
 
-// Select all links with hashes
-$('a[href*="#"]')
-// Remove links that don't actually link to anything
-    .not('[href="#"]')
-    .not('[href="#0"]')
-    .click(function(event) {
-        // On-page links
-        if (
-            location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '')
-            &&
-            location.hostname == this.hostname
-        ) {
-            // Figure out element to scroll to
+    $('a[href*=#]:not([href=#])').click(function() {
+        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'')
+            || location.hostname == this.hostname) {
+
             var target = $(this.hash);
-            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-            // Does a scroll target exist?
+            target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
             if (target.length) {
-                // Only prevent default if animation is actually gonna happen
-                event.preventDefault();
-                $('html, body').animate({
+                $('html,body').animate({
                     scrollTop: target.offset().top
-                }, 1000, function() {
-                    // Callback after animation
-                    // Must change focus!
-                    var $target = $(target);
-                    $target.focus();
-                    if ($target.is(":focus")) { // Checking if the target was focused
-                        return false;
-                    } else {
-                        $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
-                        $target.focus(); // Set focus again
-                    };
-                });
+                }, 1000);
+                return false;
             }
         }
     });
+});
