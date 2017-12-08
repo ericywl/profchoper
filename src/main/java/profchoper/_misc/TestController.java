e63403ba2a8e59f42da319457d0fb623cbcbd34a
ericywl <midnight2903@gmail.com>
2017-12-09 07:03:31 +0800

    updated student repo

--- src/main/java/profchoper/_misc/TestController.java
+++ src/main/java/profchoper/_misc/TestController.java
@@ -42,7 +42,7 @@ public class TestController {
     @GetMapping("/student")
     public String student(Map<String, Object> model) {
         LocalDate date = LocalDate.of(2017, 12, 4);
-        WeekCalendar wkCal = weekCalendarService.getStudentCalendarByCourse(50002, date);
+        WeekCalendar wkCal = weekCalendarService.getStudentCalendarByCourse("50002", date);
 
         String studentEmail = authFacade.getAuthentication().getName();
         Student student = studentService.getStudentByEmail(studentEmail);
--- src/main/java/profchoper/calendar/WeekCalendarServiceImpl.java
+++ src/main/java/profchoper/calendar/WeekCalendarServiceImpl.java
@@ -5,7 +5,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import profchoper.bookingSlot.BookingSlot;
 import profchoper.bookingSlot.BookingSlotService;
-import sun.security.x509.AVA;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
