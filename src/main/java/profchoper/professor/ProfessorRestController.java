54d76982b35a6de1eb5f10df7668c525832b0503
ericywl <midnight2903@gmail.com>
2017-12-08 16:46:23 +0800

    prof rest

--- src/main/java/profchoper/professor/ProfessorRestController.java
+++ src/main/java/profchoper/professor/ProfessorRestController.java
@@ -2,6 +2,7 @@ package profchoper.professor;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -13,7 +14,12 @@ public class ProfessorRestController {
     private ProfessorService professorService;
 
     @GetMapping("/api/professors")
-    public List<Professor> findAll() {
+    public List<Professor> getAllProfessors() {
         return professorService.getAllProfessors();
+    }
+
+    @GetMapping("/api/professors/{alias}")
+    public Professor getProfessor(@PathVariable String alias) {
+        return professorService.getProfessorByAlias(alias);
     }
 }
